b7d311cf0d28adc903e7beb425c6dc9c603c6832
Dilraj <djrandhawa15@hotmail.com>
2026-03-30 14:50:22 -0700

    add additional book entries to seedDatabase function

--- seed.js
+++ seed.js
@@ -51,11 +51,19 @@ async function seedDatabase() {
       INSERT INTO book (author_id, title, description, ISBN) VALUES
       (1, '1984', 'A dystopian novel about totalitarianism', '978-0451524935'),
       (1, 'Animal Farm', 'A satirical allegory of the Russian Revolution', '978-0451526342'),
+      (1, 'Burmese Days', 'A novel set in colonial Burma', '978-0141185362'),
       (2, 'Pride and Prejudice', 'A romantic novel of manners', '978-0141439518'),
       (2, 'Sense and Sensibility', 'A novel about two sisters', '978-0141439662'),
+      (2, 'Emma', 'A romantic comedy of errors', '978-0141439587'),
+      (2, 'Northanger Abbey', 'A parody of Gothic novels', '978-0141439570'),
       (3, 'The Great Gatsby', 'A novel about the American Dream', '978-0743273565'),
+      (3, 'Tender Is the Night', 'A novel about American expatriates', '978-0743273672'),
       (4, 'To Kill a Mockingbird', 'A classic about racial injustice', '978-0061120084'),
-      (5, 'Harry Potter and the Philosopher\'s Stone', 'The start of a magical series', '978-0747532699')
+      (4, 'Go Set a Watchman', 'A sequel to To Kill a Mockingbird', '978-0062409256'),
+      (5, 'Harry Potter and the Philosopher\'s Stone', 'The start of a magical series', '978-0747532699'),
+      (5, 'Harry Potter and the Chamber of Secrets', 'The second book in the series', '978-0747538494'),
+      (5, 'Harry Potter and the Prisoner of Azkaban', 'The third book in the series', '978-0747542155'),
+      (5, 'Harry Potter and the Goblet of Fire', 'The fourth book in the series', '978-0747546245')
     `);
 
     console.log("Database seeded successfully!");
